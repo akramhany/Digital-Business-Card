71999b1c9c8494e66eebefc16da233cb52469936
akram <akramhany65@gmail.com>
2023-11-05 01:24:05 +0200

    finish Implementing the info section

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
 import React from "react"
-import ReactDOM from "react-dom"
+import Info from "./components/Info.jsx"
 
+
+export default function App() {
+    return (
+        <>
+            <Info />
+        </>
+    )
+}
---
+++ src/components/Info.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+import personal_image from "./images/personal-image.jpg" 
+import email_logo from "./images/email.png"
+import linkedin_logo from "./images/linkedin.png"
+
+
+export default function Info() {
+    return (
+        <div className="info">
+            <img className="info-image" src={personal_image} alt="" />
+            <div className="info-data">
+                <h1 className="name">Akram Hany</h1>
+                <h3 className="title">Full Stack Developer</h3>
+                <h4 className="website-url">akramhany.website</h4>
+            </div>
+            <div className="info-buttons">
+                <button className="info-email-btn">
+                    <img src={email_logo} alt="" />
+                    Email
+                </button>
+                <button className="info-linkedin-btn">
+                    <img src={linkedin_logo} alt="" />
+                    LinkedIn
+                </button>
+            </div>
+        </div>
+    )
+}
