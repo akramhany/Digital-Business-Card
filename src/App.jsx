b0c6c0cb1801d584b737fcb76570513d93f3dc14
akram <akramhany65@gmail.com>
2023-11-05 13:08:59 +0200

    write the jsx of about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react"
 import Info from "./components/Info.jsx"
-
+import About from "./components/About.jsx"
 
 export default function App() {
     return (
         <>
             <Info />
+            <About />
         </>
     )
 }
