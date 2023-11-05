86dd5e370dc8eb30dbe3dfb3111547532186b081
akram <akramhany65@gmail.com>
2023-11-05 13:28:42 +0200

    finish implementing the Interests section

---
+++ src/components/Interests.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Interests() {
+    return (
+        <div className="interests">
+            <h1 className="interests-title">
+                Interests
+            </h1>
+            <p  className="interests-content">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis earum illum reprehenderit rem sunt, ratione veniam impedit.
+            </p>
+        </div>
+    )
+}
